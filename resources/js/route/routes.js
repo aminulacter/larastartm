61ce19796e8bfe53c50dd534cc65d0af2dc7fbbb
Aminul Acter <acterprime1@gmail.com>
2020-01-28 08:40:56 +0600

    labu jogra

---
+++ resources/js/route/routes.js
@@ -0,0 +1,8 @@
+import Dashboard from "../pages/Dashboard.vue";
+import Profile   from "../pages/Profile.vue"
+
+export let routes = [
+    {path: '/dashboard', component: Dashboard},
+    {path: '/profile', component: Profile}
+]
+ 
